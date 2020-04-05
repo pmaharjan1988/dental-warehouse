91033eae1c32cc4d5191954fe5b587baab23287f
Worawut <tonmanna@gmail.com>
2020-04-05 10:15:55 +0700

    footer update with socials

--- src/layouts/MyLayout.vue
+++ src/layouts/MyLayout.vue
@@ -1,55 +1,73 @@
 <template>
-  <q-layout view="hHh lpR fFf">
+<q-layout view="hHh lpR fFf">
     <q-header elevated>
-      <q-toolbar>
-        <q-toolbar-title>
-          <q-btn stretch class="app-name" flat size="0.9em" :label="appName" to="/" />
-        </q-toolbar-title>
-        <NavBarItems />
-        <CartButton />
-      </q-toolbar>
+        <q-toolbar>
+            <q-toolbar-title>
+                <q-btn stretch class="app-name" flat size="0.9em" :label="appName" to="/" />
+            </q-toolbar-title>
+            <NavBarItems />
+            <CartButton />
+        </q-toolbar>
     </q-header>
-    <q-footer reveal :reveal-offset="10" elevated class="bg-white text-primary text-center text-body2">
-      All Rights Reserved - {{ appName }}
+    <q-footer reveal :reveal-offset="10" elevated class="bg-white q-pa-md text-center  text-primary">
+
+        All Rights Reserved - {{ appName }}
+        <div class="row justify-center">
+            <q-icon class="text-primary q-pl-md q-ma-sm cursor-pointer" v-for="(item, index) in socialIcons" :key="index" size="20px" :name="item.icon" @click='openSocialLinks(item.link)'> </q-icon>
+        </div>
+
     </q-footer>
     <q-page-container>
-      <router-view />
+        <router-view />
     </q-page-container>
-  </q-layout>
+</q-layout>
 </template>
+
 <script>
 import BackToTop from 'vue-backtotop'
 import CartButton from '../components/CartButton.vue'
 import NavBarItems from '../components/NavBarItems.vue'
-import
-{
-  mapGetters,
+import {
+    mapGetters,
 }
 from 'vuex'
-import
-{
-  firebaseAuth
+import {
+    firebaseAuth
 }
 from 'boot/firebase'
 
-export default
-{
-  components:
-  {
-    BackToTop,
-    CartButton,
-    NavBarItems
-  },
-  mounted()
-  {
-    this.appName = process.env.APP_NAME;
-  },
-  data()
-  {
-    return {
-      appName: null,
-    }
-  },
+export default {
+    components: {
+        BackToTop,
+        CartButton,
+        NavBarItems
+    },
+    mounted() {
+        this.appName = process.env.APP_NAME;
+    },
+    data() {
+        return {
+            appName: null,
+            socialIcons: [{
+                    "icon": "fab fa-facebook-square",
+                    "link": "https://facebook.com"
+                },
+                {
+                    "icon": "fab fa-instagram",
+                    "link": "https://facebook.com"
+                },
+                {
+                    "icon": "fab fa-twitter",
+                    "link": "https://facebook.com"
+                }
+            ]
+        }
+    },
+    methods: {
+      openSocialLinks:function(link)
+      {
+         window.open(link, '_blank');  
+      }
+    },
 };
-
 </script>
